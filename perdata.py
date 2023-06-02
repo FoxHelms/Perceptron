3313f7fb8e0b7bfa081af6aa1eccd36b87d181b6
FoxHelms <113215990+FoxHelms@users.noreply.github.com>
2023-06-01 23:33:17 -0400

    15 data points. Only 985 to go...

---
+++ perdata.py
@@ -0,0 +1,18 @@
+allData = [
+    ("If you vote for Ron DeSantis, you’re fucking dead to me.", 0),
+    ("Lil X just asked if there are police cats, since there are police dogs", 0),
+    ("this man has looked the same since i was 5", 0),
+    ("Happy birthday to the legend Morgan Freeman. ", 0),
+    ("Today the award winning actor turns 86-Years-Old", 0),
+    ("I want to test what I do on YouTube on a streaming service for fun to see how it performs", 0),
+    ("BREAKING: Joe Biden falls at the Air Force Graduation", 0), 
+    ("This is Christopher Nolan’s first R-rated movie in over 20 years.", 0),
+    ("Viral video shows a raccoon getting a doughnut from Dunkin’ drive-thru", 0),
+    ("This is a scoop of cookies and cream ice cream that someone dropped on the ground.", 0),
+    ("Society needs more men like this gentleman", 0),
+    ("'CARD DECLINED' in huge bold letters on the bottle service sign is nuts omg", 0),
+    ("Bam Margera threatens to smoke crack until he’s dead unless he sees his son Phoenix", 0),
+    ("rate my setup (ignore my cat)", 0),
+    ("Zendaya via Instagram", 0),
+    ("Happy Pride, get your drag on", 0)
+    ]
