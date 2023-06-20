e37599e8a09740c90e9b8deb17dd9e1d95fabffd
FoxHelms <113215990+FoxHelms@users.noreply.github.com>
2023-06-20 12:13:50 -0400

    updating code flow

--- testforward.py
+++ testforward.py
@@ -20,28 +20,12 @@ class Perceptron:
             return 0
         return sum(i[0] * i[1] for i in zip(K, L))
 
-    def fit(self, x_list, y):
-        #n_features = len(x_list)
+    def fit(self, X, y):
 
-        self.weights = [0] * 100
-        self.bias = 0
+        return #self.weights, self.bias
 
-        #print("Xlist is " + str(x_list))
-
-        for epoch in range(self.epochs):
-
-            for i in range(len(x_list)):
-                z = self.dot(x_list, self.weights) #+ self.bias
-                y_pred = self.activation(z)
-                print("Y pred{}".format(y_pred))
-                print(y_pred[i])
-                self.weights = self.weights + self.learning_rate * (y[i] - y_pred[i]) * x_list[i]
-                self.bias = self.bias + self.learning_rate * (y[i] - y_pred[i])
-
-        return self.weights, self.bias
-
-    def predict(self, x_list):
-        z = np.dot(x_list, self.weights) + self.bias
+    def predict(self, X):
+        z = np.dot(X, self.weights) + self.bias
         return self.activation(z)
 
 
@@ -54,39 +38,34 @@ r.shuffle(allData)
 trainData = allData[:800]
 testData = allData[800:]
 
+X_train = [X[0] for X in trainData]
+y_train = [X[1] for X in trainData]
 
-#These return tuples, I don't trust it...
-#x_list_train, y_train = zip(*trainData)
-#x_list_test, y_test = zip(*testData)
+X_test = [X[0] for X in testData]
+y_test = [X[1] for X in testData]
 
-#Try this method instead
-
-x_list_train = [x_list[0] for x_list in trainData]
-y_train = [x_list[1] for x_list in trainData]
-
-x_list_test = [x_list[0] for x_list in testData]
-y_test = [x_list[1] for x_list in testData]
-
-x_list_train_int = [ss.strToInt(i) for i in x_list_train]
-x_list_test_int = [ss.strToInt(i) for i in x_list_test]
+X_train_int = [ss.strToInt(i) for i in X_train]
+X_test_int = [ss.strToInt(i) for i in X_test]
 
 
 ### IMPORTANT - You need to decide what activation function to use. 
 # Heaviside doesn't seem to work with your data type. 
 
 
-
-
-
-
 p = Perceptron(0.001, 100)
-test_weights = [0] * len(x_list_train_int)
+#test_weights = [0] * len(X_train_int)
+test_weights = [r.uniform(-0.99,0.99) for i in range(100)]
+test_dot = p.dot(X_train_int[3], test_weights)
+act = p.activation(test_dot)
+model = p.fit(X_train_int, y_train)
 
-test_dot = p.dot(x_list_train_int[3], test_weights)
+print("Test weighst: {}".format(test_weights))
 
 print("Test dot is {}".format(test_dot))
 
-model = p.fit(x_list_train_int, y_train)
+print("Activation is: {}".format(act))
 
-#pred = p.predict(x_list_test_int)
+
+
+#pred = p.predict(X_test_int)
 
---
+++ testsynt.py
@@ -0,0 +1,20 @@
+weights = [0,0,0,0,0]
+
+ugh1 = [1,2,3]
+
+
+
+ugh2 = [6,7,8,9,10]
+
+
+training_inputs = [[1,2,3],[4,5,6],[7,8,9]]
+labels = [1,0,1]
+
+for inputs, label in zip(training_inputs,labels):
+    print(inputs)
+
+
+
+
+
+
